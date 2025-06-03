aae26824c508c343c7aa0a627ac041a3313a1905
gustavogordoni <gustavogordoni1@gmail.com>
2025-06-03 11:03:49 -0300

    feat(user): create Controller UserController

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/', function (){
+    return response()->json([
+        'success' => true
+    ]);
+});
+
+// Route::get('users/',[UserController::class, 'index']);
